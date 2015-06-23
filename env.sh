125db01fac3156295e2d9971841f7bb545ba1485
ghyhm <mandy.yan.reg@gmail.com>
2015-06-23 15:58:03 +0800

    first commit

---
+++ env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export GEM_HOME=$HOME/Projects/shared/gems/ruby/2.0.0/gems/sinatra-faye-websocket-example
+export GEM_PATH=$GEM_HOME:/Library/Ruby/Gems/2.0.0
+export PATH=$PATH:$GEM_HOME/bin
